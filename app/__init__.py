135300bac92b4b34988a5e0a6905cbe1daff4225
Brandon <myboyknuckles@gmail.com>
2024-04-02 15:49:02 -0500

    Phase 2 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+from random import choice
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+@app.route('/', methods=["GET"])
+def index():
+  return render_template("index.html", tweet=choice(tweets))
+
+@app.route('/feed', methods=["GET"])
+def get_all_tweets():
+  return render_template("feed.html", all_tweets=tweets)
+
 # !!END
