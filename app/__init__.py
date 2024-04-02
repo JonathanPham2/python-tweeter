a512b8fb18407a743a2f6978078b0ae1ad4e7a87
Thanh Pham <jonathanphamw22@gmail.com>
2024-04-02 14:19:00 -0700

    phase3

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from .config import Config
 from .tweets import tweets
 from random import choice
+from app.form.tweet_form import Tweet
 
 app = Flask(__name__)
 
@@ -15,5 +16,10 @@ def index():
 @app.route('/feed', methods=["GET"])
 def get_all_tweets():
   return render_template("feed.html", all_tweets=tweets)
+
+@app.route('/new', methods=["GET", "POST"])
+def new_tweet():
+  form = Tweet()
+  return  render_template("new_tweet.html", form=form)
 
 # !!END
---
+++ app/form/tweet_form.py
@@ -0,0 +1,8 @@
+from wtforms import StringField, SubmitField
+from flask_wtf import FlaskForm
+
+
+class Tweet(FlaskForm):
+    author=StringField("Author:")
+    tweet= StringField("Tweet:")
+    submit = SubmitField("Create Tweet")
